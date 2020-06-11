2c509974e32a010b7f17af2c1ee8a08dbf9e5de6
Михаил <mikhail-gankin@yandex.ru>
2020-06-11 14:52:10 +0300

    Bag Fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ const backSide = document.querySelector('.block1__img img'),
       slidesWrapper = document.querySelectorAll('.block2__img'),
       slides = document.querySelectorAll('.block2__img img'),
       leftButton = document.querySelector('.block2__btn--left'),
-      rightButton = document.querySelector('.block2__btn--right');
+      rightButton = document.querySelector('.block2__btn--right'),
+      burger = document.querySelector('.header__burger'),
+      modal = document.querySelector('.modal'),
+      modalClose = document.querySelector('.modal__close'),
+      modalText = document.querySelector('.modal__text'),
+      closeRect1 = document.querySelector('.close__rect1'),
+      closeRect3 = document.querySelector('.close__rect3'),
+      burgerRect1 = document.querySelector('.burger__rect1'),
+      burgerRect3 = document.querySelector('.burger__rect3');
 
 let index = 0,
     backSideTimeout = 300,
@@ -70,5 +78,37 @@ const prevSlide = () => {
     }
 }
 
+const burgerAnimation = () => {
+    burgerRect1.style.transform = 'translateY(6px)';
+    burgerRect3.style.transform = 'translateY(-6px)';
+    setTimeout(() => {
+        modal.classList.remove('modal-closed');
+        modal.classList.add('modal-opened');    
+    }, 100);
+    setTimeout(() => {
+        closeRect1.style.transform = 'rotate(45deg)';
+        closeRect3.style.transform = 'rotate(-45deg)';
+    }, 300);
+    setTimeout(() => {
+        modalText.classList.add('fadeIn');
+    }, 500);
+}
+
+const modalCloseAnimation = () => {
+    modalText.classList.remove('fadeIn');
+    closeRect1.style.transform = 'rotate(0deg)';
+    closeRect3.style.transform = 'rotate(0deg)';
+    setTimeout(() => {
+        modal.classList.remove('modal-opened');
+        modal.classList.add('modal-closed');
+    }, 200);
+    setTimeout(() => {
+        burgerRect1.style.transform = 'translateY(0)';
+        burgerRect3.style.transform = 'translateY(0)';
+    }, 400);
+}
+
 rightButton.addEventListener('click',  nextSlide);
 leftButton.addEventListener('click', prevSlide);
+burger.addEventListener('click', burgerAnimation);
+modalClose.addEventListener('click', modalCloseAnimation);
